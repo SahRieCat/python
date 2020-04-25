6212f2a7bb3e9183fce3f176644890faf5f93576
unknown <sr.riemer@gmail.com>
2020-04-25 17:10:14 +0300

    02_04

--- 02_basic_datatypes/1_numbers/02_04_temp.py
+++ 02_basic_datatypes/1_numbers/02_04_temp.py
@@ -10,3 +10,10 @@ Output should read like - "81.32 degrees fahrenheit = 27.4 degrees celsius"
 
 
 '''
+
+#Write the necessary code to read a degree in Fahrenheit from the console
+temp = input("insert degree in Fahrenheit")
+tempintg = int(temp)
+#convert it to Celsius and print it to the console.
+tempincel = (tempintg - 32) * (5 / 9)
+print (tempincel)
