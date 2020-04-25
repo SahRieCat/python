99b9f1efccae46adfe4088081bb6d86be1700ebc
unknown <sr.riemer@gmail.com>
2020-04-25 17:35:51 +0300

    02_05

--- 02_basic_datatypes/1_numbers/02_05_convert.py
+++ 02_basic_datatypes/1_numbers/02_05_convert.py
@@ -9,4 +9,26 @@ Demonstrate how to:
 
     Take note of what information is lost when some conversions take place.
 
+
 '''
+# Convert an int to a float
+a = 2
+float_a = float(a)
+print (float_a)
+
+# Convert a float to an int
+b = 2.00
+int_b = int(round(b))
+print(int_b)
+
+# Perform floor division using a float and an int.
+c = a//b
+print(c)
+
+# Use two user inputted values to perform multiplication.
+user_age = input("how old are you? ")
+age = int(user_age)
+user_hight = input("what is your hight? ")
+hight = float(user_hight)
+multi = age*hight
+print("user age*hight = ",multi, "cm")
