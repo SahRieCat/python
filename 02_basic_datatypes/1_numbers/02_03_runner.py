be305c4a9ffd4211938dc8d1cb26256a94547305
unknown <sr.riemer@gmail.com>
2020-04-25 16:59:03 +0300

    02_03

--- 02_basic_datatypes/1_numbers/02_03_runner.py
+++ 02_basic_datatypes/1_numbers/02_03_runner.py
@@ -2,5 +2,10 @@
 
 If a runner runs 10 miles in 30 minutes and 30 seconds,
 What is his/her average speed in kilometers per hour? (Tip: 1 mile = 1.6 km)
-
 '''
+
+#30 minutes and 30 seconds is 0.505 of an hour
+mile = 1
+kilometer = mile*1.6
+print (10*kilometer//0.505)
+
