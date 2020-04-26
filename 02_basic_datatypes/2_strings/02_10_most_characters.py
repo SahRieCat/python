2a0ca2ee30f9cbfb38a8b4d89384ae59bcd4557f
unknown <sr.riemer@gmail.com>
2020-04-26 16:45:33 +0300

    02_10

--- 02_basic_datatypes/2_strings/02_10_most_characters.py
+++ 02_basic_datatypes/2_strings/02_10_most_characters.py
@@ -11,3 +11,36 @@ CHALLENGE: Can you edit to script to print only the string with the most charact
            into the topic "Conditionals" to solve this challenge.
 
 '''
+#input1
+usr1 = input("type something")
+#input2
+usr2 = input("type something")
+#input3
+usr3 = input("type something")
+#count1
+cou1 = (len(usr1))
+
+#count2
+cou2 = (len(usr2))
+#count3
+cou3 = (len(usr3))
+#printall
+print(cou1,",",usr1)
+print(cou2,",",usr2)
+print(cou3,",",usr3)
+
+
+#print only the string with the most characters
+
+
+if cou1 >= cou2:
+   print(usr1)
+elif cou1 >= cou3:
+       print(usr1)
+
+elif cou2 >= cou3:
+       print(usr2)
+else:
+   print(usr3)
+
+
