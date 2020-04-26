ff9aa1c90a034730508ede6f9842c9f59027b331
unknown <sr.riemer@gmail.com>
2020-04-26 14:39:56 +0300

    02_07

--- 02_basic_datatypes/2_strings/02_07_replace.py
+++ 02_basic_datatypes/2_strings/02_07_replace.py
@@ -8,3 +8,11 @@ Result: #ore python progra##ing please
 
 '''
 
+#String input:
+usrsent = input("write a sentence")
+#Symbol input:
+usrsym = input("insert a symbol")
+#the letter we want to replace
+fl = usrsent[0]
+#Result: #ore python progra##ing pleasen
+print(usrsent.replace(fl,usrsym ))
