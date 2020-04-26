eb4c60998919fcd585e2fcb627b7e575a5aadc05
unknown <sr.riemer@gmail.com>
2020-04-26 15:34:39 +0300

    02_09

--- 02_basic_datatypes/2_strings/02_09_vowel.py
+++ 02_basic_datatypes/2_strings/02_09_vowel.py
@@ -6,3 +6,11 @@ CHALLENGE: Can you change the script so that it counts the occurrence of each in
            in the string and print a count for each of them?
 
 '''
+#input
+usrinp = input("enter sentence")
+#count
+print("A/a = ",*map(usrinp.lower().count, "Aa"))
+print("E/e = ",*map(usrinp.lower().count, "Ee"))
+print("I/i = ",*map(usrinp.lower().count, "Ii"))
+print("O/o = ",*map(usrinp.lower().count, "Oo"))
+print("U/u = ",*map(usrinp.lower().count, "Uu"))
